06925de931b58f9e0d53b3b0e610e6a7aa67e6d3
jittagorn <jittagorn@geniustree.co.th>
2014-01-30 10:14:20 +0700

    call syntax timeout 3000

--- src/main/webapp/library.js
+++ src/main/webapp/library.js
@@ -607,6 +607,8 @@ eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a
 
 
 
-setTimeout(function(){
+var higlighterTimeout = setTimeout(function(){
+   window.clearTimeout(higlighterTimeout);
+   
    SyntaxHighlighter.all(); 
 }, 3000);
