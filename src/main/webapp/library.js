631bea88fabe8e80fafb0b431d1a56f55c6b2995
jittagorn <jittagorn@geniustree.co.th>
2014-01-30 10:12:54 +0700

    call syntax

--- src/main/webapp/library.js
+++ src/main/webapp/library.js
@@ -607,5 +607,6 @@ eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a
 
 
 
-
-SyntaxHighlighter.all();
+setTimeout(function(){
+   SyntaxHighlighter.all(); 
+}, 3000);
